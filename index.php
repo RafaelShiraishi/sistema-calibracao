3a9e0a56beb332582927fdb2f910832c87af2ce3
RafaelShiraishi <rafaelshiraishi37@gmail.com>
2026-07-07 14:46:11 +0000

    Corrige index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ echo "<h1>Se você está lendo isso, o seu servidor PHP está 100% funcionando n
 echo "<p>Para testar a conexão com o banco de dados, acesse o link adicionando /conexao.php no final da URL.</P>";
 ?>
 <?php
-(teste railway)
 
 header('Content-Type: text/plain');
 
