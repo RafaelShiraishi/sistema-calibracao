dbd78fdac885b6a6c1b7595e77169cfc7032b46b
RafaelShiraishi <rafaelshiraishi37@gmail.com>
2026-07-07 13:40:27 +0000

    teste railway

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+echo "<h1>Se você está lendo isso, o seu servidor PHP está 100% funcionando na nuvem!</h1>";
+echo "<p>Para testar a conexão com o banco de dados, acesse o link adicionando /conexao.php no final da URL.</P>";
+?>
+<?php
+(teste railway)
 
 header('Content-Type: text/plain');
 
@@ -7,4 +12,5 @@ echo "PHP_VERSION = " . PHP_VERSION . "\n";
 echo "PWD = " . getcwd() . "\n";
 echo "FILES:\n";
 
+print_r(scandir('.'));
 print_r(scandir('.'));
